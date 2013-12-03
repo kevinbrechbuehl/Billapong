15782f4d1f307e73f5515a299f15cadcc6a8ee33
Kevin Brechbühl <mail@aquasonic.ch>
2013-12-03 15:50:58 +0100

    Add basic contract, implementation and test.

---
+++ Billapong.Contract/Service/IAdministrationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billapong.Contract.Service
+{
+    [ServiceContract(Name = "BillapongAdministration", Namespace = Globals.ServiceContractNamespaceName)]
+    public interface IAdministrationService
+    {
+        [OperationContract(Name = "Test")]
+        string Test(string name);
+    }
+}
