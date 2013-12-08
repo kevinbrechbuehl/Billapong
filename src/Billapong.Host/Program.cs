a17041c1ccd4889fc63be17ac3cd17c33d18d8bc
Kevin Brechbühl <mail@aquasonic.ch>
2013-12-08 12:07:43 +0100

    Add console host for testing.

---
+++ src/Billapong.Host/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Billapong.Implementation;
+
+namespace Billapong.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = null;
+            try
+            {
+                host = new ServiceHost(typeof(AdministrationService));
+                host.Open();
+
+                Console.WriteLine("Administration Service is running");
+                Console.ReadKey();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                if (host != null)
+                {
+                    host.Close();
+                }
+            }
+        }
+    }
+}
