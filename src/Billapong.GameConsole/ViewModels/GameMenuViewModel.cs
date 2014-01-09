37c66c891905ac24b65e8fc464c1483e31628eaf
Pascal Mathys <pascal.mathys@rootix.ch>
2014-01-09 18:48:49 +0100

    Removed old test code

--- src/Billapong.GameConsole/ViewModels/GameMenuViewModel.cs
+++ src/Billapong.GameConsole/ViewModels/GameMenuViewModel.cs
@@ -37,34 +37,6 @@
         }
 
         /// <summary>
-        /// Gets the log command.
-        /// </summary>
-        /// <value>
-        /// The log command.
-        /// </value>
-        public DelegateCommand LogCommand
-        {
-            get
-            {
-                return new DelegateCommand(this.Log);
-            }
-        }
-
-        /// <summary>
-        /// Gets the get maps command.
-        /// </summary>
-        /// <value>
-        /// The get maps command.
-        /// </value>
-        public DelegateCommand GetMapsCommand
-        {
-            get
-            {
-                return new DelegateCommand(this.GetMaps);
-            }
-        }
-
-        /// <summary>
         /// Opens the map selection.
         /// </summary>
         /// <param name="parameter">The parameter.</param>
@@ -72,50 +44,6 @@
         {
             var viewModel = new MapSelectionViewModel();
             this.OnWindowContentSwitchRequested(new WindowContentSwitchRequestedEventArgs(viewModel));
-        }
-
-        /// <summary>
-        /// Test logging method
-        /// </summary>
-        /// <param name="parameter">The parameter.</param>
-        private void Log(object parameter)
-        {
-            var exception = new Exception();
-            try
-            {
-                int.Parse("lala");
-            }
-            catch (Exception ex)
-            {
-                exception = ex;
-            }
-            
-            Tracer.Debug("this is log via tracer");
-            Tracer.Info("this is a info via tracer");
-            Tracer.Warn("WARNING");
-            Tracer.Error("WOHO, error", exception);
-
-            var messages = Tracer.GetLogMessages();
-            MessageBox.Show(string.Format("Current number of log messages saved on server: {0}", messages.Count()));
-        }
-
-        /// <summary>
-        /// Gets the maps.
-        /// </summary>
-        /// <param name="parameter">The parameter.</param>
-        private void GetMaps(object parameter)
-        {
-            var client = new GameConsoleServiceClient();
-            var maps = client.GetMaps().ToList();
-
-            var mapsInfo = new List<string> { "Number of maps: " + maps.Count };
-
-            foreach (var map in maps)
-            {
-                mapsInfo.Add(string.Format("Name: {0}, Windows: {1}, Holes: {2}", map.Name, map.Windows.Count, map.Windows.Select(window => window.Holes.Count).Sum()));
-            }
-
-            MessageBox.Show(string.Join(Environment.NewLine, mapsInfo));
         }
     }
 }
