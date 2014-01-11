d656a0e1c2e830631fca378cc841c03700dbef76
Kevin Brechbühl <mail@aquasonic.ch>
2014-01-11 20:08:23 +0100

    Validate log message repository test.

--- tests/Billapong.DataAccessTest/Tracing/TracingRepositoryTest.cs
+++ tests/Billapong.DataAccessTest/Tracing/TracingRepositoryTest.cs
@@ -1,4 +1,4 @@
-﻿namespace Billapong.DataAccessTest
+﻿namespace Billapong.DataAccessTest.Tracing
 {
     using System;
     using System.Linq;
@@ -10,14 +10,15 @@
     /// Tests The LogMessage class
     /// </summary>
     [TestClass]
-    public class LogMessageTest
+    public class TracingRepositoryTest
     {
         /// <summary>
         /// Tests the repository
         /// </summary>
         [TestMethod]
-        public void RepositoryTest()
+        public void LogMessageRepositoryTest()
         {
+            // arrange
             var repository = new Repository<LogMessage>();
             var count = repository.Get().Count();
 
@@ -26,13 +27,16 @@
                 LogLevel = 1,
                 Message = "This is a test debug message.",
                 Component = "Unit Test",
+                Sender = "Unit Test Method",
                 Timestamp = DateTime.Now
             };
 
+            // act
             repository.Add(message);
             repository.Save();
-
             var newCount = repository.Get().Count();
+
+            // assert
             Assert.IsTrue(count + 1 == newCount, "Log message was not saved correctly.");
         }
     }
