e32afb9b06b1136c1b7ebb5c60d960b466ad2799
Kevin Brechbühl <mail@aquasonic.ch>
2014-01-21 20:08:06 +0100

    Add project for map editor.

---
+++ src/Billapong.MapEditor/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿namespace Billapong.MapEditor
+{
+    using System.Windows;
+    using Contract.Data.Tracing;
+    using Core.Client.Tracing;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Raises the <see cref="E:System.Windows.Application.Startup" /> event.
+        /// </summary>
+        /// <param name="e">A <see cref="T:System.Windows.StartupEventArgs" /> that contains the event data.</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            this.DispatcherUnhandledException += this.App_DispatcherUnhandledException;
+
+            Tracer.Initialize(Component.MapEditor);
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Windows.Application.Exit" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.Windows.ExitEventArgs" /> that contains the event data.</param>
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Tracer.Shutdown();
+            base.OnExit(e);
+        }
+
+        /// <summary>
+        /// Handles the DispatcherUnhandledException event of the App control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Windows.Threading.DispatcherUnhandledExceptionEventArgs"/> instance containing the event data.</param>
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message, "Fehler");
+            e.Handled = true;
+        }
+    }
+}
