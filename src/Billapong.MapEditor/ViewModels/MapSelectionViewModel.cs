591781a60976f1234acea6c9c4388d043bfdaf02
Kevin Brechbühl <mail@aquasonic.ch>
2014-01-28 08:49:18 +0100

    Create MVVM structure and basic map selection view.

---
+++ src/Billapong.MapEditor/ViewModels/MapSelectionViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace Billapong.MapEditor.ViewModels
+{
+    using Core.Client.UI;
+
+    public class MapSelectionViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// The is data loading
+        /// </summary>
+        private bool isDataLoading = false;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the view data is loading.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if the view data is loading; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsDataLoading
+        {
+            get
+            {
+                return this.isDataLoading;
+            }
+
+            set
+            {
+                this.isDataLoading = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
