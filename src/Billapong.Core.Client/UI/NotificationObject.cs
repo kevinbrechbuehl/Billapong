f7894cd0c715b3bd8779ae16f789871b6468ed11
Pascal Mathys <pascal.mathys@rootix.ch>
2014-01-28 11:04:58 +0100

    Separated the notification code from the base viewmodel

---
+++ src/Billapong.Core.Client/UI/NotificationObject.cs
@@ -0,0 +1,25 @@
+﻿namespace Billapong.Core.Client.UI
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// Enables property notification on objects
+    /// </summary>
+    public abstract class NotificationObject : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        /// <summary>
+        /// Called when a property changed.
+        /// </summary>
+        /// <param name="propertyName">Name of the property.</param>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- src/Billapong.Core.Client/UI/ViewModelBase.cs
+++ src/Billapong.Core.Client/UI/ViewModelBase.cs
@@ -1,25 +1,9 @@
 ﻿namespace Billapong.Core.Client.UI
 {
-    using System.ComponentModel;
-    using System.Runtime.CompilerServices;
-
     /// <summary>
     /// Provides basic functionality to view models
     /// </summary>
-    public abstract class ViewModelBase : INotifyPropertyChanged
+    public abstract class ViewModelBase : NotificationObject
     {
-        /// <summary>
-        /// Occurs when a property value changes.
-        /// </summary>
-        public event PropertyChangedEventHandler PropertyChanged = delegate { };
-
-        /// <summary>
-        /// Called when a property changed.
-        /// </summary>
-        /// <param name="propertyName">Name of the property.</param>
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
-        {
-            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-        }
     }
 }
