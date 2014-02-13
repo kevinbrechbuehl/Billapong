e00f742779db49fd2d70d24d0903f8184853a796
Kevin Brechbühl <mail@aquasonic.ch>
2014-02-13 20:52:45 +0100

    Refactore mapcontroller

--- src/Billapong.Core.Server/Map/MapEditor.cs
+++ src/Billapong.Core.Server/Map/MapEditor.cs
@@ -3,13 +3,25 @@
     using System.Collections.Generic;
     using Contract.Service;
 
+    /// <summary>
+    /// Class for storing all callbacks to a specific map.
+    /// </summary>
     public class MapEditor
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MapEditor"/> class.
+        /// </summary>
         public MapEditor()
         {
             this.Callbacks = new List<IMapEditorCallback>();
         }
-        
+
+        /// <summary>
+        /// Gets the callbacks.
+        /// </summary>
+        /// <value>
+        /// The callbacks.
+        /// </value>
         public IList<IMapEditorCallback> Callbacks { get; private set; }
     }
 }
