e44b6bc20d34d58414e8aa14cf70b94867f9bf75
Kevin Brechbühl <mail@aquasonic.ch>
2014-02-25 10:30:22 +0100

    Add controller with todo to add the highscores to the admin.

---
+++ src/Billapong.Administration/Controllers/HighScoreController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Billapong.Administration.Controllers
+{
+    public class HighScoreController : Controller
+    {
+        //
+        // GET: /HighScore/
+        public ActionResult Index()
+        {
+            // todo (breck1): implement view -> zuerst auflistung von allen maps und allen usern und auf weiterer Seite dann die scores dafür
+            
+            return null;
+        }
+	}
+}
