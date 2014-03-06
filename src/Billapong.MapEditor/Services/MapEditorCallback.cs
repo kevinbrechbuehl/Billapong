3a61b6cb11afedc3a2bc1ae2eb836c989f073859
Kevin Brechbühl <mail@aquasonic.ch>
2014-03-06 19:43:06 +0100

    Improve tracing for map editor.

--- src/Billapong.MapEditor/Services/MapEditorCallback.cs
+++ src/Billapong.MapEditor/Services/MapEditorCallback.cs
@@ -8,6 +8,9 @@ namespace Billapong.MapEditor.Services
 {
     using System.ServiceModel;
     using System.Windows;
+
+    using Billapong.Core.Client.Tracing;
+
     using Contract.Data.Map;
     using Contract.Service;
     using Core.Client.Helper;
@@ -31,29 +34,34 @@ namespace Billapong.MapEditor.Services
         public void UpdateName(string name)
         {
             var args = new UpdateNameEventArgs(name);
+            Tracer.Debug(string.Format("MapEditViewModel :: Name updated callback retrieved (Name={0})", args.Name));
             ThreadContext.InvokeOnUiThread(() => this.NameUpdated(this, args));
         }
 
         public void UpdateIsPlayable(bool isPlayable)
         {
             var args = new UpdateIsPlayableEventArgs(isPlayable);
+            Tracer.Debug(string.Format("MapEditViewModel :: Is playable callback retrieved (Flag={0})", args.IsPlayable));
             ThreadContext.InvokeOnUiThread(() => this.IsPlayableUpdated(this, args));
         }
 
         public void AddWindow(long windowId, int coordX, int coordY)
         {
             var args = new GameWindowEventArgs(windowId, coordX, coordY);
+            Tracer.Debug(string.Format("MapEditViewModel :: Window added callback retrieved ({0})", args));
             ThreadContext.InvokeOnUiThread(() => this.WindowAdded(this, args));
         }
 
         public void RemoveWindow(long windowId, int coordX, int coordY)
         {
             var args = new GameWindowEventArgs(windowId, coordX, coordY);
+            Tracer.Debug(string.Format("MapEditViewModel :: Window removed callback retrieved ({0})", args));
             ThreadContext.InvokeOnUiThread(() => this.WindowRemoved(this, args));
         }
 
         public void AddHole(long windowId, int windowX, int windowY, long holeId, int holeX, int holeY)
         {
+            Tracer.Debug(string.Format("MapEditViewModel :: Hole added callback retrieved ({0})", args));
             var args = new GameHoleClickedEventArgs(windowId, windowX, windowY, holeId, holeX, holeY);
             ThreadContext.InvokeOnUiThread(() => this.HoleAdded(this, args));
         }
@@ -61,6 +69,7 @@ namespace Billapong.MapEditor.Services
         public void RemoveHole(long windowId, int windowX, int windowY, long holeId)
         {
             var args = new GameHoleClickedEventArgs(windowId, windowX, windowY, holeId);
+            Tracer.Debug(string.Format("MapEditViewModel :: Hole removed callback retrieved ({0})", args));
             ThreadContext.InvokeOnUiThread(() => this.HoleRemoved(this, args));
         }
     }
