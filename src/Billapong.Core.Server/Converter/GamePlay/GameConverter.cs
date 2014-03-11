72dc3efca6f0062ed4636ee0da8b04f0d8b23851
Kevin Brechbühl <mail@aquasonic.ch>
2014-03-11 08:19:01 +0100

    Fix NullReferenceException when converting open game to contract.

--- src/Billapong.Core.Server/Converter/GamePlay/GameConverter.cs
+++ src/Billapong.Core.Server/Converter/GamePlay/GameConverter.cs
@@ -14,14 +14,24 @@
 
         public static Contract.Data.GamePlay.Game ToContract(this Server.GamePlay.Game source)
         {
-            return new Contract.Data.GamePlay.Game()
+            var game = new Contract.Data.GamePlay.Game()
             {
                 Id = source.Id,
                 Map = source.Map.Name,
-                Player1Name = source.Players[0].Name,
-                Player2Name = source.Players[1].Name,
                 Status = (Contract.Data.GamePlay.GameStatus)source.Status
             };
+
+            if (source.Players != null && source.Players[0] != null)
+            {
+                game.Player1Name = source.Players[0].Name;
+            }
+
+            if (source.Players != null && source.Players[1] != null)
+            {
+                game.Player2Name = source.Players[1].Name;
+            }
+
+            return game;
         }
     }
 }
