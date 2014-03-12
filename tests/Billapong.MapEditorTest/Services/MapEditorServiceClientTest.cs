21bd78baa0a8b37b077869374fea947e860d41c9
Kevin Brechbühl <mail@aquasonic.ch>
2014-03-12 19:16:36 +0100

    Add unit test over wcf service.

---
+++ tests/Billapong.MapEditorTest/Services/MapEditorServiceClientTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billapong.MapEditorTest.Services
+{
+    using System.ServiceModel;
+    using Billapong.Core.Server.Services;
+    using Billapong.MapEditor.Services;
+    using Billapong.Tests.Common;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MapEditorServiceClientTest : TestBase
+    {
+        private readonly static ServiceHost MapEditorService = new ServiceHost(typeof(MapEditorService));
+
+        [ClassInitialize]
+        public static void StartService(TestContext testContext)
+        {
+            MapEditorService.Open();
+        }
+
+        [ClassCleanup]
+        public static void EndService()
+        {
+            if (MapEditorService.State == CommunicationState.Opened)
+            {
+                MapEditorService.Close();
+            }
+        }
+
+        [TestMethod]
+        public void GetMapsTest()
+        {
+            // arrange
+            var service = new MapEditorServiceClient();
+
+            // act
+            var maps = service.GetMaps().ToList();
+
+            // asset
+            Assert.IsTrue(maps.Count > 1);
+            Assert.IsTrue(maps.First().Windows.Count == 1);
+            Assert.IsTrue(maps.First().Windows.First().Holes.Count == 3);
+        }
+    }
+}
