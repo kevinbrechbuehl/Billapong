281e0da80f7f421d0d17e58aa47c280f2d2aa7bc
Kevin Brechbühl <mail@aquasonic.ch>
2014-03-18 13:29:08 +0100

    Add login to map editor unit test.

--- tests/Billapong.MapEditorTest/Services/MapEditorServiceClientTest.cs
+++ tests/Billapong.MapEditorTest/Services/MapEditorServiceClientTest.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 namespace Billapong.MapEditorTest.Services
 {
     using System.ServiceModel;
+    using Billapong.Contract.Data.Authentication;
+    using Billapong.Core.Client.Authentication;
     using Billapong.Core.Server.Services;
     using Billapong.MapEditor.Services;
     using Billapong.Tests.Common;
@@ -17,10 +19,30 @@ namespace Billapong.MapEditorTest.Services
     {
         private readonly static ServiceHost MapEditorService = new ServiceHost(typeof(MapEditorService));
 
+        private readonly static ServiceHost AuthenticationService = new ServiceHost(typeof(AuthenticationService));
+
         [ClassInitialize]
         public static void StartService(TestContext testContext)
         {
             MapEditorService.Open();
+            AuthenticationService.Open();
+        }
+
+        [TestMethod]
+        public void GetMapsTest()
+        {
+            // arrange
+            var authenticatioProxy = new AuthenticationServiceClient();
+            var sessionId = authenticatioProxy.Login("editor", "editor", Role.Editor);
+            var mapEditorProxy = new MapEditorServiceClient(sessionId);
+
+            // act
+            var maps = mapEditorProxy.GetMaps().ToList();
+
+            // asset
+            Assert.IsTrue(maps.Count > 1);
+            Assert.IsTrue(maps.First().Windows.Count == 1);
+            Assert.IsTrue(maps.First().Windows.First().Holes.Count == 3);
         }
 
         [ClassCleanup]
@@ -30,22 +52,11 @@ namespace Billapong.MapEditorTest.Services
             {
                 MapEditorService.Close();
             }
-        }
 
-        [TestMethod]
-        public void GetMapsTest()
-        {
-            // arrange
-            // todo (breck1): fix test because we need a valid session id here
-            var service = new MapEditorServiceClient(Guid.NewGuid());
-
-            // act
-            var maps = service.GetMaps().ToList();
-
-            // asset
-            Assert.IsTrue(maps.Count > 1);
-            Assert.IsTrue(maps.First().Windows.Count == 1);
-            Assert.IsTrue(maps.First().Windows.First().Holes.Count == 3);
+            if (AuthenticationService.State == CommunicationState.Opened)
+            {
+                AuthenticationService.Close();
+            }
         }
     }
 }
